843602d66e0f0b0608ff61e898c733774ef7540d
Adam Hutchinson <adjohu@gmail.com>
2015-09-04 14:39:58 +0100

    Different approach

--- src/react-error-suppression.js
+++ src/react-error-suppression.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
-export default function wrapComponent(Component, logErrors = false) {
-  return class extends Component {
-    render() {
-      try {
-        return super.render();
-      } catch (e) {
-        if (logErrors) {
-          console.log(e);
-        }
+let errorLogging = false;
 
-        return <div />;
+class WrappedComponent extends React.Component {
+  constructor(options) {
+    super(options);
+
+    const {render} = this;
+
+    this.render = function () {
+      try {
+        return render.apply(this, ...arguments);
+      } catch (e) {
+        if (errorLogging) {
+          console.log('error in component', e);
+        }
+        return <div />
       }
     }
   }
 }
+
+const ReactWithErrorSuppression = Object.assign({} , React,  {
+  Component: WrappedComponent,
+  enableErrorLogging() {
+    errorLogging = true;
+  }
+});
+
+export default ReactWithErrorSuppression;
